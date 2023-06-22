d4f2099efb1a3ee894a1a68f12127d1152aeb6e8
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-22 10:30:47 +0530

    [Rohit]Add: // add all files

---
+++ EmployeeWagePro/EmployeeAttendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagePro
+{
+    public class EmployeeAttendance
+    {
+        public static void EmployeeCheck()
+        {
+            int IS_FULL_TIME = 1;
+            //random function
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWagePro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagePro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation Problem");
+            //UC1-EmployeeAttendance
+            EmployeeAttendance.EmployeeCheck();
+            Console.ReadLine();
+        }
+    }
+}
